71e7313326668de1727d56eea16cdbeec97075ce
Nikolay Podvorchan <hello@rtnikki.me>
2021-03-01 20:17:46 +0100

    Frontend first commit

---
+++ frontend/app.py
@@ -0,0 +1,5 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.markdown("## Here will be Vincent Van Bot")
+components.iframe('https://assets.pinterest.com/ext/embed.html?id=524317581589830707', width=345, height=548)
