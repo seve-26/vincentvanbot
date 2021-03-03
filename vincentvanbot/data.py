22fbadb41ea3689ae849fc62cc9d4d74579482ba
Severo Bregante <severo.bregante@gmail.com>
2021-03-03 16:09:05 +0100

    Finalized functions to push/pull pickle

--- vincentvanbot/data.py
+++ vincentvanbot/data.py
@@ -2,7 +2,7 @@ import os
 import pandas as pd
 from vincentvanbot.preprocessing.utils import get_jpg_link
 from vincentvanbot.preprocessing.image import create_pickle_db, pickle_upload
-from vincentvanbot.params import IMAGES_PATH, PICKLE_PATH, BUCKET_NAME, BUCKET_PICKLE_PATH
+from vincentvanbot.params import IMAGES_PATH, PICKLE_PATH_ROOT, BUCKET_NAME, BUCKET_PICKLE_FOLDER
 from vincentvanbot.utils import download_single_image
 from google.cloud import storage
 
@@ -13,13 +13,13 @@ def get_data_locally(nrows=10):
     """Return df with initial database and jpg image"""
     path = os.path.join(os.path.dirname(__file__),'..','raw_data','catalog.csv')
     # encode to take care of non-ASCII characters such as 'ö'
-    df = pd.read_csv(path, nrows=nrows, encoding= 'unicode_escape')
+    df = pd.read_csv(path, encoding= 'unicode_escape')
 
     # transform html link to jpg
     df['URL'] = df['URL'].map(get_jpg_link)
 
     # keep only paintings
-    df = df[df['FORM'] == 'painting']
+    df = df[df['FORM'] == 'painting'].head(nrows)
 
     return df
 
@@ -30,22 +30,30 @@ def download_images_locally(df):
     print(f'\nDownloading images to {IMAGES_PATH}...')
     df = df.progress_apply(download_single_image,axis=1)
 
-def get_pickle(source='gcp'):
+def get_pickle(size=100, source='gcp'):
     """Gets pickle file from source and returns images df"""
     client = storage.Client()
+    if size:
+        PICKLE_PATH = PICKLE_PATH_ROOT+'_'+str(size)+'.pkl'
+        local_pickle_name = f'flat_resized_images_{str(size)}.pkl'
+    else:
+        PICKLE_PATH = PICKLE_PATH_ROOT+'.pkl'
+        local_pickle_name = 'flat_resized_images.pkl'
+    
     if source == 'local':
         path = PICKLE_PATH
     elif source == 'gcp':
-        path = f"gs://{BUCKET_NAME}/{BUCKET_PICKLE_PATH}"
+        path = f"gs://{BUCKET_NAME}/{BUCKET_PICKLE_FOLDER}/{local_pickle_name}"
     img_df = pd.read_pickle(path)
 
     return img_df
 
 
 if __name__ == '__main__':
-    df = get_data_locally(nrows=100_000)
+    nrows=100
+    df = get_data_locally(nrows=nrows)
     download_images_locally(df)
-    create_pickle_db(path=IMAGES_PATH, dim=(36,42))
-    pickle_upload(rm=True)
-    # img_df = get_pickle(source='gcp')
-    # print(img_df.shape)
+    create_pickle_db(size=nrows, path=IMAGES_PATH, dim=(36,42))
+    pickle_upload(size=nrows, rm=True)
+    img_df = get_pickle(size=nrows, source='gcp')
+    print(img_df.shape)
--- vincentvanbot/preprocessing/image.py
+++ vincentvanbot/preprocessing/image.py
@@ -1,15 +1,14 @@
-from vincentvanbot.params import IMAGES_PATH
 import os
 import pandas as pd
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from google.cloud import storage
 from termcolor import colored
-from vincentvanbot.params import BUCKET_NAME, PICKLE_PATH
+from vincentvanbot.params import IMAGES_PATH, BUCKET_NAME, PICKLE_PATH_ROOT
 
 from tqdm import tqdm
 
 
-def create_pickle_db(path=IMAGES_PATH, dim=(36,42)):
+def create_pickle_db(size=100, path=IMAGES_PATH, dim=(36,42)):
     """For each image in path, resizes it to the given dim, transforms it into a flat vector
     and stores in a df. Then dumps it into a pickle file"""
 
@@ -26,18 +25,27 @@ def create_pickle_db(path=IMAGES_PATH, dim=(36,42)):
     # normalize and save df to pickle file
     img_db.sort_index(inplace=True)
     img_db = img_db / 255
-    img_db.to_pickle(PICKLE_PATH)
+    if size:
+        img_db.to_pickle(PICKLE_PATH_ROOT+'_'+str(size)+'.pkl')
+    else:
+        img_db.to_pickle(PICKLE_PATH_ROOT+'.pkl')
 
 
-def pickle_upload(rm=False):
+def pickle_upload(size=100, rm=False):
     """Upload pickle file to google cloud"""
     client = storage.Client().bucket(BUCKET_NAME)
 
-    local_pickle_name = 'flat_resized_images.pkl'
+    if size:
+        PICKLE_PATH = PICKLE_PATH_ROOT+'_'+str(size)+'.pkl'
+        local_pickle_name = f'flat_resized_images_{str(size)}.pkl'
+    else:
+        PICKLE_PATH = PICKLE_PATH_ROOT+'.pkl'
+        local_pickle_name = 'flat_resized_images.pkl'
+    
     storage_location = f"data/{local_pickle_name}"
     blob = client.blob(storage_location)
     blob.upload_from_filename(PICKLE_PATH)
-    print(colored(f"\n=> flat_resized_images.pkl uploaded to bucket {BUCKET_NAME} inside {storage_location}",
+    print(colored(f"\n=> {local_pickle_name} uploaded to bucket {BUCKET_NAME} inside {storage_location}",
                   "green"))
     if rm:
         os.remove(PICKLE_PATH)
