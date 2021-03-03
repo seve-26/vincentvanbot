bf8d91865b87bed29ba4adf0b3570319f75ba17f
Julia Schmitt <julia.s.schmitt@gmx.de>
2021-03-03 21:11:20 +0100

    add URL_function to model.py

--- vincentvanbot/model.py
+++ vincentvanbot/model.py
@@ -4,19 +4,16 @@ import pickle
 import matplotlib.pyplot as plt
 from sklearn.neighbors import NearestNeighbors
 
-
-#open feature vectors with pickle file
-
-# take random subsample of images of size num_images
-# num_images = 100
+from vincentvanbot.data import get_data_locally
+from vincentvanbot.preprocessing.pipeline import build_pipe
 
 
 
-def flatten_images():
+def flatten_images(df_transformed):
     """Returns dataframe with a column of flattened image vectors of shape (453600,)"""
     images = df_transformed['IMAGE'].iloc[:,] # Read scaled images as numpy arrays
     flat_images = [image.flatten() for image in images]
-    X_flattened = pd.DataFrame(np.vstack(flatten_images()))
+    X_flattened = pd.DataFrame(np.vstack(flatten_images(df_transformed)))
 
     return X_flattened
 
@@ -29,21 +26,28 @@ def train_KNN_model(X_flattened, n_neighbors):
     return knn_model
 
 
-def return_closest_images(df, knn_model, X_test):
+def return_closest_images(df_transformed, knn_model, X_test):
     """Filters original dataframe with indices and returns reduced dataframe"""
     ind_list = list(knn_model.kneighbors(X_test,n_neighbors=5)[1][0])[1:] # get n closest points, unpack indices to a list
-    df_closest_images = df.iloc[ind_list, :] # filter original df with indices. Sort by distance - default??
-    #retrieve respective URLS
+    df_closest_images = df.iloc[ind_list, :] # filter original df with indices
 
     return df_closest_images
 
 
 def get_URL(ind_list, df_transformed):
-    pass
+    """Returns list of URLs of closest images"""
+    lst_URL = []
+    for i in range(len(ind_list)):
+        closest_img = df_transformed.iloc[i] # original dataframe
+        img_URL = closest_img[6]
+        lst_URL.append(img_URL)
+
+    return lst_URL
 
 
-# function which gets df with details of closest images (author, titel, date etc)
 def img_details():
+    """function which gets df with details of closest images (author, titel, date etc)"""
     # df_transformed.iloc[ind_df.iloc[1]].drop(columns=["URL", "IMAGE"])
     # convert to list or dict
     pass
+
