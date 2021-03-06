7e8a77e432a687b87ebf9aea1862af72eb1086ff
Severo Bregante <severo.bregante@gmail.com>
2021-03-06 10:37:48 +0100

    Merged model.py with model-seve.py. Changed preprocessing/utils.py to avoid importing tensorflow, open-cv and matplotlib

--- vincentvanbot/model.py
+++ vincentvanbot/model.py
@@ -1,42 +1,96 @@
-import numpy as np
-import pandas as pd
-import pickle
-import matplotlib.pyplot as plt
+import os
 from sklearn.neighbors import NearestNeighbors
+import joblib
+import pandas as pd
+from google.cloud import storage
+from vincentvanbot.params import BUCKET_NAME, BUCKET_INITIAL_DATASET_FOLDER
+from vincentvanbot.preprocessing.utils import get_jpg_link
 
 
-def flatten_images(df_transformed):
-    """Returns dataframe with a column of flattened image vectors of shape (453600,)"""
-    images = df_transformed['IMAGE'].iloc[:,] # Read scaled images as numpy arrays
-    flat_images = [image.flatten() for image in images]
-    X_flattened = pd.DataFrame(np.vstack(flatten_images(df_transformed)))
+def train_model(df_transformed):
+    """Takes preprocessed train data as df. Returns fitted KNN model 
+    and train data image indexes (used then to refer back to initial database).
+    Saves locally model and indexes."""
+    knn_model = NearestNeighbors().fit(df_transformed)
+    
+    joblib.dump(knn_model,'model.joblib')
+    joblib.dump(df_transformed.index,'train_indexes.joblib')
 
-    return X_flattened
+def save_model_to_cloud(rm=False):
+    """Uploads fitted model and related indexes to GCloud."""
+    client = storage.Client().bucket(BUCKET_NAME)
+    
+    for filename in ['model.joblib','train_indexes.joblib']:
+        storage_location = f"predict/{filename}"
+        blob = client.blob(storage_location)
+        blob.upload_from_filename(filename)
+        print(f"=> {filename} uploaded to bucket {BUCKET_NAME} inside {storage_location}")
+    if rm:
+        os.remove('model.joblib')
+        os.remove('train_indexes.joblib')
+
+def get_closest_images_indexes(user_input_transformed, nsimilar=3, rm=True):
+    """Takes user_input_transformed as np.array. Downloads fitted knn model and related indexes.
+    Returns indexes of nsimilar closest images"""
+    client = storage.Client().bucket(BUCKET_NAME)
+    
+    # download model
+    local_name = 'model_10000.joblib'
+    storage_location = f"predict/{local_name}"
+    blob = client.blob(storage_location)
+    blob.download_to_filename(local_name)
+    print(f"=> {local_name} downloaded from storage")
+    model = joblib.load(local_name)
+    
+    # download indexes
+    local_name = 'train_indexes_10000.joblib'
+    storage_location = f"predict/{local_name}"
+    blob = client.blob(storage_location)
+    blob.download_to_filename(local_name)
+    print(f"=> {local_name} downloaded from storage")
+    indexes = joblib.load(local_name)
+    
+    if rm:
+        os.remove('model_10000.joblib')
+        os.remove('train_indexes_10000.joblib')
+    
+    index_neighbors = model.kneighbors(user_input_transformed, n_neighbors=nsimilar)[1][0]
+    
+    return [int(indexes[i]) for i in list(index_neighbors)]
+
+def get_info_from_index(indexes, all_info=False):
+    """From given image indexes, gets initial dataset from gcloud
+    and returns respective information (urls, etc.)"""
+    client = storage.Client()
+    
+    dataset_filename = 'catalog.csv'
+    path = f"gs://{BUCKET_NAME}/{BUCKET_INITIAL_DATASET_FOLDER}/{dataset_filename}"
+    
+    df = pd.read_csv(path, encoding= 'unicode_escape')
+    df['URL'] = df['URL'].map(get_jpg_link)
+    
+    urls = [df.iloc[i]['URL'] for i in indexes]
+    
+    if not all_info:
+        return urls
+    
+    # get additional info
+    titles = [df.iloc[i]['TITLE'] for i in indexes]
+    authors = [df.iloc[i]['AUTHOR'] for i in indexes]
+    
+    return urls, titles, authors
 
 
-def return_closest_images(df_transformed, X_test):
-    """Filters original dataframe with indices and returns reduced dataframe"""
-    knn_model = NearestNeighbors(n_neighbors=5).fit(X_flattened)
-    ind_list = list(knn_model.kneighbors(X_test,n_neighbors=5)[1][0])[1:] # get n closest points, unpack indices to a list
-    df_closest_images = df.iloc[ind_list, :] # filter original df with indices
+if __name__=='__main__':
+    from vincentvanbot.preprocessing.utils import preprocess_image
+    # from vincentvanbot.data import get_joblib_data
+    path = os.path.join(os.path.dirname(__file__),'..','notebooks','example-input.jpg')
+    # print(path)
+    user_img = preprocess_image(path,dim=(100,100))
+    # train_df = get_joblib_data()
 
-    return df_closest_images
-
-
-def get_URL(ind_list, df_transformed):
-    """Returns list of URLs of closest images"""
-    lst_URL = []
-    for i in range(len(ind_list)):
-        closest_img = df_transformed.iloc[i] # original dataframe
-        img_URL = closest_img[6]
-        lst_URL.append(img_URL)
-
-    return lst_URL
-
-
-def img_details():
-    """function which gets df with details of closest images (author, titel, date etc)"""
-    # df_transformed.iloc[ind_df.iloc[1]].drop(columns=["URL", "IMAGE"])
-    # convert to list or dict
-    pass
-
+    # train_model(train_df)
+    # save_model_to_cloud(rm=True)
+    indexes = get_closest_images_indexes(user_img)
+    urls = get_info_from_index(indexes)
+    print(urls)
--- vincentvanbot/preprocessing/utils.py
+++ vincentvanbot/preprocessing/utils.py
@@ -1,11 +1,15 @@
-import cv2
-import matplotlib.pyplot as plt
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import requests
+from PIL import Image
+import io
+from pathlib import Path
+import numpy as np
 
-def resize_image(img,width=420,height=360):
-    dim = (width, height)
+_PIL_INTERPOLATION_METHODS = {
+        'nearest': Image.NEAREST,
+        'bilinear': Image.BILINEAR,
+        'bicubic': Image.BICUBIC,
+    }
 
-    return cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
 
 def get_jpg_link(html_link):
     """Tranform the html_link of the image to its respective jpg_link"""
@@ -13,11 +17,39 @@ def get_jpg_link(html_link):
     
     return jpg_link
 
+def load_img(path,target_size=(100,100), interpolation='nearest'):
+    """read the path/image in bytes, and store it in img"""
+    if isinstance(path, io.BytesIO):
+        img = Image.open(path)
+    elif isinstance(path, (Path, bytes, str)):
+        if isinstance(path, Path):
+            path = str(path.resolve())
+        with open(path, 'rb') as f:
+            img = Image.open(io.BytesIO(f.read()))
+    else:
+        raise TypeError('path should be path-like or io.BytesIO'
+                        ', not {}'.format(type(path)))
+    
+    # resize the image
+    if target_size is not None:
+        width_height_tuple = (target_size[1], target_size[0])
+        resample = _PIL_INTERPOLATION_METHODS[interpolation]
+        img = img.resize(width_height_tuple, resample)
+    
+    return img
+
+def img_to_array(img, dtype='float32'):
+    """Given a PIL image object, returns it's vectorial representation"""
+    x = np.asarray(img, dtype=dtype)
+    if len(x.shape) == 2:
+            x = x.reshape((x.shape[0], x.shape[1], 1))
+    return x
+
 def jpg_to_array(jpg_link):
     """Given an image jpg_link, it returns its vectorial representation"""
-    img_vector = plt.imread(jpg_link, format='jpg')
+    img = Image.open(requests.get(test_link, stream=True).raw)
     
-    return img_vector
+    return img_to_array(img)
 
 def preprocess_image(img, dim=(36,42)):
     """Takes img (either bytes or local path), returns np.array of flat,resized,normalized img"""
