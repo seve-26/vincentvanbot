6361c90e0fbc61557d58da5c3eb5526d4dfe6c42
Severo Bregante <severo.bregante@gmail.com>
2021-03-06 10:54:56 +0100

    Refactored code for preprocessing pipeline of recommender system

--- vincentvanbot/preprocessing/pipeline.py
+++ vincentvanbot/preprocessing/pipeline.py
@@ -1,12 +1,25 @@
-from sklearn.pipeline import Pipeline
-#from model-seve import train_model
-from vincentvanbot.preprocessing.featureencoder import transformer
+"""This file defines the preprocessing pipeline for the recommender system.
+Includes different steps to be conducted on different columns of the starting database"""
+
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
 
 
 def build_pipe():
-    pipe = Pipeline(steps =[
-        ('t', transformer),
-        ('model', knn_model)
-        ])
+    """Build preprocessing pipeline for the initial database"""
+    cols_to_encode = ["TYPE", "SCHOOL"]
+    encoder = OneHotEncoder(sparse = False)
 
-    return pipe
+    preproc_pipe = ColumnTransformer([
+        ('onehot', encoder, cols_to_encode)
+    ], remainder='drop')
+
+    return preproc_pipe
+
+
+if __name__ == '__main__':
+    from vincentvanbot.data import get_data_locally
+    df = get_data_locally(nrows=100_000)
+    pipe = build_pipe()
+    array_transformed = pipe.fit_transform(df)
+    print(array_transformed.shape)
