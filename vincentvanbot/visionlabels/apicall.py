fb9bb3429a772382bc3bff9170173ee0dcc7d620
Severo Bregante <severo.bregante@gmail.com>
2021-03-07 15:15:40 +0100

    Refactored function to create labels df. Added option to feed the Vision API an image file uploaded from locally (probably faster than using the URL)

--- vincentvanbot/visionlabels/apicall.py
+++ vincentvanbot/visionlabels/apicall.py
@@ -1,4 +1,5 @@
 from google.cloud import vision
+import io
 
 LABELS_SELECTION = [
     'Illustration', 'History', 'Drawing', 'Mythology', 'Flower','Stock photography', 'Holy places',
@@ -8,7 +9,7 @@ LABELS_SELECTION = [
     ]
 
 
-def get_labels_url(uri, max_results, proba_threshold=0.6):
+def get_labels_from_url(uri, max_results, proba_threshold=0.6):
     """Takes in uri (i.e. jpg link to an image). Returns dictionary having as keys the identified
     labels, and as values their related proba.
     In particular:
@@ -34,10 +35,42 @@ def get_labels_url(uri, max_results, proba_threshold=0.6):
     
     return labels_dict
 
+def get_labels_from_local_path(path, max_results, proba_threshold=0.6):
+    """Takes in path (i.e. path to an image). Returns dictionary having as keys the identified
+    labels, and as values their related proba.
+    In particular:
+    - only labels with proba > proba_threshold
+    - a maximum of max_results different labels
+    - only labels manually defined in LABELS_SELECTION"""
+
+    # open image file
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    # connect to google vision
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image(content=content)
+
+    # get labels for input path
+    response = client.label_detection(image=image, max_results=max_results)
+    labels = response.label_annotations
+
+    # create dict having as keys all labels, as values their related probas
+    # filter dict to only include labels in LABELS_SELECTION and proba > proba_threshold
+    labels_dict = {}
+    for label in labels:
+        if label.description in LABELS_SELECTION and label.score > proba_threshold:
+            labels_dict[label.description] = label.score
+    
+    return labels_dict
+
 
 if __name__ == '__main__':
-    from vincentvanbot.data import get_data_locally
-    df = get_data_locally(nrows=100_000)
-    url_list = list(df.sample(3)['URL'])
+    # from vincentvanbot.data import get_data_locally
+    # df = get_data_locally(nrows=100_000)
+    # url_list = list(df.sample(3)['URL'])
+    # print([get_labels_from_url(url,25) for url in url_list])
 
-    print([get_labels_url(url,25) for url in url_list])
+    from os.path import join, dirname
+    path = join(dirname(__file__),'..','..','raw_data','images','0.jpg')
+    print(get_labels_from_local_path(path,25))
--- vincentvanbot/visionlabels/labelsdf.py
+++ vincentvanbot/visionlabels/labelsdf.py
@@ -1,26 +1,52 @@
-import pandas as pd
-from vincentvanbot.visionlabels.apicall import get_labels_url
+from vincentvanbot.visionlabels.apicall import get_labels_from_url, get_labels_from_local_path
+from os.path import join, dirname
+
+from tqdm import tqdm
+tqdm.pandas(bar_format='{l_bar}{bar:30}{r_bar}{bar:-10b}')
 
 
-def get_labels_df(df, max_results, proba_threshold=0.6):
-    """Get labels for each url of the given df."""
-    list_of_dicts = []
-
-    for i in range(len(df)):
-        url = df.iloc[i]["URL"]
-        labels_dict = get_labels_url(url, max_results=max_results, proba_threshold=proba_threshold)
-        labels_dict['Index'] = i
-        list_of_dicts.append(labels_dict)
-
-    final_df = pd.DataFrame(list_of_dicts)
-    final_df.fillna(0, inplace= True)
-    final_df.set_index('Index', inplace = True)
+def get_labels_row(row, max_results, proba_threshold,source):
+    if source == 'url':
+        labels_dict = get_labels_from_url(
+            row['URL'],
+            max_results=max_results,
+            proba_threshold=proba_threshold
+            )
+    elif source == 'local':
+        path = join(dirname(__file__),'..','..','raw_data','images',str(row.name) + '.jpg')
+        labels_dict = get_labels_from_local_path(
+            path,
+            max_results=max_results,
+            proba_threshold=proba_threshold
+            )
+    else:
+        raise ValueError("Unknown source for images.")
     
-    return final_df
+    row.drop(row.index,inplace=True)
+    for label, proba in labels_dict.items():
+        row[label] = proba
+    return row
+
+def get_labels_df(df, max_results, source='url', proba_threshold=0.6):
+    """Get labels for each url of the given df."""
+    labels_df = df.copy()
+
+    print("\nExtracting labels from Google Vision API...")
+    labels_df = labels_df.progress_apply(
+        get_labels_row,
+        axis=1,
+        max_results=max_results,
+        proba_threshold=proba_threshold,
+        source=source
+        )
+
+    labels_df.fillna(0, inplace= True)
+    
+    return labels_df
 
 
 if __name__ == '__main__':
     from vincentvanbot.data import get_data_locally
     df = get_data_locally(5)
-    labels_df = get_labels_df(df,25)
+    labels_df = get_labels_df(df,25, source='local')
     print(labels_df)
