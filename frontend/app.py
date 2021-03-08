bf453384326b45571ada38c67330a461c75eb582
Nikolay Podvorchan <hello@rtnikki.me>
2021-03-08 19:58:52 +0100

    Frontend updated for n pics and recommender

--- frontend/app.py
+++ frontend/app.py
@@ -2,22 +2,62 @@ import streamlit as st
 import streamlit.components.v1 as components
 import requests
 import os
+import io
 from os.path import join, dirname
 from dotenv import load_dotenv
 from PIL import Image
 import copy
 import pandas as pd
 from google.cloud import storage
+import math
 
+# FUNCTIONS
+# Custom image resize function
+def resize_image(image, max_h = 500):
+    pic_w, pic_h = image.size
+    if pic_h <= max_h:
+        return image
+    
+    ratio = pic_w / pic_h
+    pic_h = max_h
+    pic_w = int(pic_h * ratio)
+    img_resized = image.resize((pic_w, pic_h))
+    
+    return img_resized
+
+def render_response():
+    st.markdown("")
+            
+    # Rendering in a grid
+    n_items = len(response)
+    rows = math.ceil(n_items / 3)
+                         
+    for n_row in range(rows):
+        cols = st.beta_columns(3)
+        # API stricture: {img_url, html_url, author, title, created, museum}
+        for n_col in range(3 if n_row < rows - 1 else n_items - n_row * 3):
+            response_item = response[3 * n_row + n_col]
+            pic_received = Image.open(requests.get(response_item['img_url'], stream = True).raw)
+            pic_resized = resize_image(pic_received)
+            cols[n_col].markdown(f"""*Title:* [{response_item['title']}]({response_item['html_url']})  
+                                         *Author:* {response_item['author']}  
+                                         *Created:* {response_item['created']}  
+                                         *Museum: * {response_item['museum']}""")
+            cols[n_col].image(pic_resized)
+            cols[n_col].markdown("")
+
+
+# MAIN SECTION
+# Downloading env variables
 env_path = join(dirname(__file__),'.env')
 load_dotenv(dotenv_path=env_path)
 
 # Download our catalogue
-if not os.path.exists('catalog.csv'):
+if not os.path.exists(join(dirname(__file__),'catalog.csv')):
     storage_client = storage.Client('vincent-van-bot')
     bucket = storage_client.get_bucket('vincent-van-bot-bucket')
     blob = bucket.blob('data/catalog.csv')
-    blob.download_to_filename('catalog.csv')
+    blob.download_to_filename(join(dirname(__file__),'catalog.csv'))
 
 # Open our catalogue + add first empty row to have empty pre-selection for a user
 db = pd.read_csv('catalog.csv', encoding='unicode_escape')
@@ -26,8 +66,7 @@ db['Title_author_date'] = db['TITLE'] + '; ' + db['AUTHOR'] + '; ' + db['DATE']
 empty_row = pd.DataFrame([[" "] * len(db.columns)], columns=db.columns)
 db = empty_row.append(db, ignore_index=True)
 
-#api_url = os.getenv('API_URL')
-api_url = 'http://127.0.0.1:8000/uploadfile/'
+api_url = os.getenv('API_URL')
 
 # Rendering the page starts here
 st.set_page_config(page_title = 'Vincent van Bot', layout = 'wide')
@@ -47,44 +86,60 @@ with st.beta_expander("Input zone", expanded = True):
         st.markdown("#### or")
         st.markdown("")
         
-        pic_name = st.selectbox("Tell us what is your favorite painting", db['Title_author_date'])
+        pic_name = st.selectbox("Tell us what is your favorite painting (and remove your downloaded pic beforehand, if any)", db['Title_author_date'])
         
-        #st.button("Send")
+    with image_col:
+        st.markdown("Your picture / favorite painting:")
+        img_location = st.empty()
 
     if img:
         img_copy = copy.deepcopy(img)
         img_resized = Image.open(img_copy)
+        img_resized = resize_image(img_resized)
+        img_location.image(img_resized)
         
-        pic_w, pic_h = img_resized.size
-        ratio = pic_w / pic_h
-        long_vertical_pic = pic_h > 500
-        
-        if pic_h > 500:
-            pic_h = 500
-            pic_w = int(pic_h * ratio)
-            img_resized = img_resized.resize((pic_w, pic_h))
-        
-        with image_col:
-            st.markdown("Your picture / favorite painting:")
-            st.image(img_resized, use_column_width = not long_vertical_pic)
-        
-    
-if img:
-    files = {"file": (img.name, img, img.type)}
-    form_data = {"nsimilar" : nsimilar}
-    response = requests.post(api_url, files=files, data=form_data).json()
-        
-    if response:
-        st.markdown("")
-        st.markdown("#### So you will probably like these paintings!")
-        # API stricture: {img_url, html_url, author, title, created, museum}
-        for response_item in response:
-            st.image(response_item['img_url'])
-            st.text(response_item['title'])
+    elif pic_name != " ":
+        url_to_fetch = db.loc[db['Title_author_date'] == pic_name, 'URL'].values[0].replace('html','art', 1).replace('html','jpg')
+        user_painting_bytes = requests.get(url_to_fetch).content
+        user_painting = Image.open(io.BytesIO(user_painting_bytes))
+        img_resized = resize_image(user_painting)
+        img_location.image(img_resized)
     else:
-        st.markdown("#### We were not able to find similar paintings 😭")
+        pass
+        
+                          
+# Just adding some spacing           
+st.markdown("")
+
+# Rendering output
+with st.beta_expander("So you will probably like these paintings:", expanded = (img is not None or pic_name != " ")):
+    
+    if img:
+        files = {"file": (img.name, img, img.type)}
+        form_data = {"nsimilar" : nsimilar}
+        response = requests.post(api_url, files=files, data=form_data).json()
             
-    img = None
+        if response:
+            render_response()
+        else:
+            st.markdown("#### Looks like we were not able to find similar paintings 😭")
+        
+    elif pic_name != " ":
+        files = {"file": ("user_pic.jpg", user_painting_bytes, 'image/jpeg')}
+        form_data = {"nsimilar" : nsimilar}
+        response = requests.post(api_url, files=files, data=form_data).json()
+        
+        if response:
+            render_response()
+        else:
+            st.markdown("#### Looks like we were not able to find similar paintings 😭")
+        
+    else:
+        pass
+        
+        
+
+    
         
     
     
