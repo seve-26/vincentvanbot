389c288225a3b161134f02d01c95002eecf67044
Severo Bregante <severo.bregante@gmail.com>
2021-03-15 16:14:35 +0100

    Removed redundant files

--- setup.py
+++ setup.py
@@ -13,5 +13,4 @@ setup(name='vincentvanbot',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/vincentvanbot-run'],
       zip_safe=False)
